25cf96d742a6d94aaf247cedf196303c6e16884a
NINU <ninuolaalaka@gmail.com>
2026-02-08 15:37:29 +0400

    Update checkout-uae.js

--- al-lara-website/api/checkout-uae.js
+++ al-lara-website/api/checkout-uae.js
@@ -1,4 +1,7 @@
-
+export const config = {
+  runtime: "node.js",
+  regions: ["bah1"]
+};
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
